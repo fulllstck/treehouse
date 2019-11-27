60dae13fb8654e2cab8404ecb58a9ecc0577f552
Adam Denes <fullstack.ad@gmail.com>
2019-11-27 14:10:45 +0000

    Adding arrow functions

---
+++ arrow_function/refactor.js
@@ -0,0 +1,13 @@
+const multiplyByTen = num => 10 * num;
+  
+const printToTen = () => {
+	for (let i = 1; i <= 10; i++) {
+		console.log(i);
+	}
+}
+
+const addUs = (num1, num2) => console.log(num1 + num2);
+
+console.log(multiplyByTen(2));
+printToTen();
+addUs(2,3);
